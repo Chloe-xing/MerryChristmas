58c0489ed2e0d601c615c0417c5d38d7cf13b828
Chloe <Administrator@WIN-RULS6TKS2PM>
2022-12-09 13:41:04 +0800

    plus版

--- script.js
+++ script.js
@@ -1,230 +1,215 @@
-MorphSVGPlugin.convertToPath("polygon");
+MorphSVGPlugin.convertToPath('polygon');
 var xmlns = "http://www.w3.org/2000/svg",
-  xlinkns = "http://www.w3.org/1999/xlink",
-  select = function (s) {
+  xlinkns = "http://www.w3.org/1999/xlink"
+select = function(s) {
     return document.querySelector(s);
   },
-  selectAll = function (s) {
+  selectAll = function(s) {
     return document.querySelectorAll(s);
   },
-  pContainer = select(".pContainer"),
-  mainSVG = select(".mainSVG"),
-  star = select("#star"),
-  sparkle = select(".sparkle"),
-  tree = select("#tree"),
+  pContainer = select('.pContainer'),
+  mainSVG = select('.mainSVG'),
+  star = select('#star'),
+  sparkle = select('.sparkle'),
+  tree = select('#tree'),
   showParticle = true,
-  particleColorArray = [
-    "#E8F6F8",
-    "#ACE8F8",
-    "#F6FBFE",
-    "#A2CBDC",
-    "#B74551",
-    "#5DBA72",
-    "#910B28",
-    "#910B28",
-    "#446D39",
-  ],
-  particleTypeArray = ["#star", "#circ", "#cross", "#heart"],
-  // particleTypeArray = ['#star'],
+  //particleColorArray = ['#69D2E7', '#A7DBD8', '#E0E4CC', '#F38630', '#FA6900'],
+  //particleColorArray = ['#7FBBEF','#D5780F','#381502','#FDED56','#7D1607','#F0C495','#C2293B','#53AB61','#343DE1','#FCD422'],
+  //particleColorArray = ['#FFFCF9', '#339B8B', '#B74551','#C89568','#CC9966', '#336666', '#669966', '#CC9966', '#CC3333', '#339B8B', '#B74551'],
+  particleColorArray = ['#E8F6F8', '#ACE8F8', '#F6FBFE','#A2CBDC','#B74551', '#5DBA72', '#910B28', '#910B28', '#446D39'],
+  particleTypeArray = ['#star','#circ','#cross','#heart'],
+  particleTypeArray = ['#star'],
   particlePool = [],
   particleCount = 0,
-  numParticles = 201;
+  numParticles = 200
 
-gsap.set("svg", {
-  visibility: "visible",
+
+TweenMax.set('svg', {
+  visibility: 'visible'
+})
+
+TweenMax.set(sparkle, {
+	transformOrigin:'50% 50%',
+	y:-100
+})
+
+var treePath = MorphSVGPlugin.pathDataToBezier(select('.treePath').getAttribute('d'), {
+  offsetX: 0,
+  offsetY: -5
+});
+var treeBottomPath = MorphSVGPlugin.pathDataToBezier(select('.treeBottomPath').getAttribute('d'), {
+  offsetX: 0,
+  offsetY: -5
 });
 
-gsap.set(sparkle, {
-  transformOrigin: "50% 50%",
-  y: -100,
-});
+//console.log(starPath.length)
+var mainTl = new TimelineMax({delay:2, repeat:-1}), starTl;
 
-let getSVGPoints = (path) => {
-  let arr = [];
-  var rawPath = MotionPathPlugin.getRawPath(path)[0];
-  rawPath.forEach((el, value) => {
-    let obj = {};
-    obj.x = rawPath[value * 2];
-    obj.y = rawPath[value * 2 + 1];
-    if (value % 2) {
-      arr.push(obj);
-    }
-    //console.log(value)
-  });
 
-  return arr;
-};
-let treePath = getSVGPoints(".treePath");
+//tl.seek(100).timeScale(1.82)
 
-var treeBottomPath = getSVGPoints(".treeBottomPath");
+function randomBetween(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-var mainTl = gsap.timeline({ delay: 0, repeat: 100 }),
-  starTl;
-
-function flicker(p) {
-  gsap.killTweensOf(p, { opacity: true });
-  gsap.fromTo(
-    p,
-    {
-      opacity: 1,
-    },
-    {
-      duration: 0.07,
-      opacity: Math.random(),
-      repeat: -1,
-    }
-  );
+function flicker(p){
+  
+  //console.log("flivker")
+  TweenMax.killTweensOf(p, {opacity:true});
+  TweenMax.fromTo(p, 0.07,{
+    alpha:1
+  }, {
+    alpha:Math.random(),
+    //ease: RoughEase.ease.config({ template: Power0.easeNone, strength: 3, points: 6, taper: "both", randomize: true, clamp: false}),
+    repeat:-1
+  })
 }
 
 function createParticles() {
-  var i = numParticles,
-    p,
-    particleTl,
-    step = numParticles / treePath.length,
-    pos;
+  
+  //var step = numParticles/starPath.length;
+  //console.log(starPath.length)
+  var i = numParticles, p, particleTl, step = numParticles/treePath.length, pos;
   while (--i > -1) {
-    p = select(particleTypeArray[i % particleTypeArray.length]).cloneNode(true);
+    
+    p = select(particleTypeArray[i%particleTypeArray.length]).cloneNode(true);
     mainSVG.appendChild(p);
-    p.setAttribute("fill", particleColorArray[i % particleColorArray.length]);
-    p.setAttribute("class", "particle");
+    p.setAttribute('fill', particleColorArray[i % particleColorArray.length]);
+    p.setAttribute('class', "particle");   
     particlePool.push(p);
     //hide them initially
-    gsap.set(p, {
-      x: -100,
-      y: -100,
-      transformOrigin: "50% 50%",
-    });
+    TweenMax.set(p, {
+                 x:-100, 
+                 y:-100,
+   transformOrigin:'50% 50%'
+                 })
+    
+    
+
   }
+
 }
-
-var getScale = gsap.utils.random(0.5, 3, 0.001, true);
-
-function playParticle(p) {
-  if (!showParticle) {
-    return;
-  }
-  var p = particlePool[particleCount];
-  gsap.set(p, {
-    x: gsap.getProperty(".pContainer", "x"),
-    y: gsap.getProperty(".pContainer", "y"),
-    scale: getScale(),
-  });
-  var tl = gsap.timeline();
-  tl.to(p, {
-    duration: gsap.utils.random(0.61, 6),
-    physics2D: {
-      velocity: gsap.utils.random(-23, 23),
-      angle: gsap.utils.random(-180, 180),
-      gravity: gsap.utils.random(-6, 50),
-    },
-    scale: 0,
-    rotation: gsap.utils.random(-123, 360),
-    ease: "power1",
-    onStart: flicker,
-    onStartParams: [p],
-    onRepeat: (p) => {
-      gsap.set(p, {
-        scale: getScale(),
-      });
-    },
-    onRepeatParams: [p],
-  });
-
-  particleCount++;
-  particleCount = particleCount >= numParticles ? 0 : particleCount;
+function getScale(){
+  return randomBetween(5, 20)/10;
 }
-
-function drawStar() {
-  starTl = gsap.timeline({ onUpdate: playParticle });
-  starTl
-    .to(".pContainer, .sparkle", {
-      duration: 6,
-      motionPath: {
-        path: ".treePath",
-        autoRotate: false,
-      },
-      ease: "linear",
-    })
-    .to(".pContainer, .sparkle", {
-      duration: 1,
-      onStart: function () {
-        showParticle = false;
-      },
-      x: treeBottomPath[0].x,
-      y: treeBottomPath[0].y,
-    })
-    .to(
-      ".pContainer, .sparkle",
-      {
-        duration: 2,
-        onStart: function () {
-          showParticle = true;
-        },
-        motionPath: {
-          path: ".treeBottomPath",
-          autoRotate: false,
-        },
-        ease: "linear",
-      },
-      "-=0"
-    )
-    .from(
-      ".treeBottomMask",
-      {
-        duration: 2,
-        drawSVG: "0% 0%",
-        stroke: "#FFF",
-        ease: "linear",
-      },
-      "-=2"
+function playParticle(p){
+  if(!showParticle){return};
+  var p = particlePool[particleCount]
+ TweenMax.set(p, {
+      //attr:{
+          x: pContainer._gsTransform.x,
+          y: pContainer._gsTransform.y,
+          scale:getScale()
+      //}
+    }
     );
+var tl = new TimelineMax();
+  tl.to(p, randomBetween(10, 60)/10, {
+      //paused:true,
+      physics2D: {
+        velocity: randomBetween(-13, 16),
+        angle:randomBetween(-180, 180),
+        gravity:randomBetween(-13, 44)
+      },
+
+      scale:0,
+      rotation:randomBetween(-123,360),
+      //skewY:(Math.random() * 180),
+      ease: Power1.easeOut,
+      onStart:flicker,
+      onStartParams:[p],
+      //repeat:-1,
+      onRepeat: function(p) {        
+        TweenMax.set(p, {         
+            scale:getScale()
+        })
+
+      },
+      onRepeatParams: [p]
+
+    });
+  
+
+  //
+  //particlePool[particleCount].play();
+  particleCount++;
+  //mainTl.add(tl, i / 1.3)
+  particleCount = (particleCount >=numParticles) ? 0 : particleCount
+  
+}
+
+function drawStar(){
+  
+  starTl = new TimelineMax({onUpdate:playParticle})
+  starTl.to([pContainer, sparkle], 6, {
+    bezier: {
+      type: "cubic",
+      values: treePath,
+      autoRotate: false
+    },
+    ease: Linear.easeNone
+  })  
+  .to([pContainer, sparkle], 1, {
+    onStart:function(){showParticle = false},
+    x:treeBottomPath[0].x,
+    y:treeBottomPath[0].y
+  })
+  .to([pContainer, sparkle], 2, {
+    onStart:function(){showParticle = true},
+    bezier: {
+      type: "cubic",
+      values: treeBottomPath,
+      autoRotate: false
+    },
+    ease: Linear.easeNone    
+  },'-=0')
+.from('.treeBottomMask', 2, {
+  drawSVG:'0% 0%',
+  stroke:'#FFF',
+  ease:Linear.easeNone
+},'-=2')  
+  
+
+  //TweenMax.staggerTo(particlePool, 2, {})
+  
+}
+
+function textAnim(){
+  
 }
 
 createParticles();
 drawStar();
+//ScrubGSAPTimeline(mainTl)
 
-mainTl
-  .from([".treePathMask", ".treePotMask"], {
-    duration: 6,
-    drawSVG: "0% 0%",
-    stroke: "#FFF",
-    stagger: {
-      each: 6,
-    },
-    duration: gsap.utils.wrap([6, 1, 2]),
-    ease: "linear",
-  })
-  .from(
-    ".treeStar",
-    {
-      duration: 3,
-      scaleY: 0,
-      scaleX: 0.15,
-      transformOrigin: "50% 50%",
-      ease: "elastic(1,0.5)",
-    },
-    "-=4"
-  )
+mainTl.staggerFrom(['.treePathMask','.treePotMask','.treeBottomMask'], 6, {
+  drawSVG:'0% 0%',
+  stroke:'#FFF',
+  cycle:{
+    duration:[6, 1,2]
+  },
+  ease:Linear.easeNone
+},6)
+.from('.treeStar', 3, {
+  //skewY:270,
+  scaleY:0,
+  scaleX:0.15,
+  transformOrigin:'50% 50%',
+  ease:Elastic.easeOut.config(1,0.5)
+},'-=4')
+  .to(['.treeStarOutline', '.messageText'], 1, {
+    alpha:1,
+    ease:RoughEase.ease.config({ template:  Power0.easeNone, strength: 2, points: 16, taper: "none", randomize: true, clamp: false})
+  },'-=3')
+ .to(sparkle, 3, {
+    alpha:0,
+    ease:RoughEase.ease.config({ template:  Power0.easeNone, strength: 2, points: 100, taper: "both", randomize: true, clamp: false})
+  },'-=5')
+.to('.whole', 2, {
+	alpha:0,
+	ease:Sine.easeIn
+},'+=2')
 
-  .to(
-    ".sparkle",
-    {
-      duration: 3,
-      opacity: 0,
-      ease: "rough({strength: 2, points: 100, template: linear, taper: both, randomize: true, clamp: false})",
-    },
-    "-=0"  
-  )
-  .to(
-    ".treeStarOutline",
-    {
-      repeat: 3,
-      duration: 1,
-      opacity: 1,
-      ease: "rough({strength: 2, points: 16, template: linear, taper: none, randomize: true, clamp: false})",
-    },
-    "+=0"
-  );
-
-mainTl.add(starTl, 0);
-gsap.globalTimeline.timeScale(1.5);
+mainTl.add(starTl, 0)
+TweenMax.globalTimeScale(1.5);
+//ScrubGSAPTimeline(mainTl)
